5b35e3fad0e59472a3111746dd8988f439ca1943
Jonas Barnert <job5461@thi.de>
2025-03-25 09:30:56 +0100

    .

--- main.c
+++ main.c
@@ -2,6 +2,25 @@
 #include "_mcpr_stm32f407.h"
 #include <inttypes.h>
 
+void udelay(int delay)
+{
+	for(int i = 0; i < delay; i++)
+	{
+		
+	}
+}
+
+void LEDs_InitPorts(){
+	//Ports mit MODER konfigureieren
+}
+
+void LCD_Output16BitWord(uint16_t data){
+	//16bit wort umschreiben in richtiges format
+}
+
+void LEDs_Write (uint16_t data){
+	//ports manipulieren
+}
 
 int main(void)
 {
@@ -12,17 +31,18 @@ int main(void)
 	// Peripheral GPIOD einschalten
 	RCC->AHB1ENR |= 1<<3|1;
 
-	// Orange LED (Port D13) auf Ausgang schalten
-	GPIOD->MODER |= 0x04000000; //1<<26;
+	// Orange LED (Port D12) auf Ausgang schalten
+	GPIOD->MODER |= 1<<24; //1<<26;
 	GPIOD->ODR |= 1<<13;
-	
 	while( 1 ) {
 		if( (GPIOA->IDR & 1) != 0) { 
-			GPIOD->ODR |= 1<<13; 
+			GPIOD->ODR |= 1<<12; 
+			udelay(3500000); // 500ms an 
+			GPIOD->ODR &= ~(1<<12); 
+			udelay(3500000); // 500ms aus
 		} else { 
-			GPIOD->ODR &= 0xDFFF; // ~(1<<13); 
+			GPIOD->ODR &= ~(1<<12); 
 		}
 	}
-		
 }
 
